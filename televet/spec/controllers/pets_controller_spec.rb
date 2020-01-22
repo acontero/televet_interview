4fac1a16d6437c2bc1755aa45bb008c0bf64e62c
Angelica <acontero@gmail.com>
2020-01-22 14:28:39 -0500

    Adding controller tests for pets controller

--- televet/spec/controllers/pets_controller_spec.rb
+++ televet/spec/controllers/pets_controller_spec.rb
@@ -1,21 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe PetsController, type: :controller do
+  # Could mock objects using something like FactoryGirl gem:
   # before(:each) do
   #     @project  = create :project
   # end
-  #
-  # it 'does return '
-  # post(:create, params: { book: { title: "Love Hina" }})
-  #
-  #   # Asserts that the controller tried to redirect us to
-  #   # the created book's URI.
-  #   assert_response :found
+
+  describe 'GET #index' do
+    it 'does show a paginated list of pets' do
+      get :index, params: {page: 1}
+        expect(response).to have_http_status(:success)
+        payload = JSON.parse(response.body)
+        #puts payload
+        size = payload["pets"].size
+        expect(size) == 10
+    end
+
+    it 'does show a empty list of pets when page is out of range' do
+      get :index, params: {page: 17}
+        expect(response).to have_http_status(:success)
+        payload = JSON.parse(response.body)
+        puts payload
+        size = payload["pets"].size
+        expect(size) == 0
+    end
+  end
 
   describe 'GET #show' do
-    it 'does show the pet with the given id' do
-      get :show
-      expect(response).to be_ok
+    it 'does SHOW the pet with the given id' do
+      get :show, params: {id: 1}
+        expect(response).to have_http_status(:success)
+    end
+
+    it 'does return 404 when pet with given id is not found' do
+      get :show, params: {id: 150}
+        expect(response).to have_http_status(:not_found)
     end
   end
 end
